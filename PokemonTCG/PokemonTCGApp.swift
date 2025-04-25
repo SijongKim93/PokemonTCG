d4005bc59672ddddaf34155be5eb919dd5178df5
SijongKim93 <rlatlwhd456@gmail.com>
2025-04-26 00:05:04 +0900

    [Setting] 초기 세팅 시작

---
+++ PokemonTCG/PokemonTCGApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonTCGApp.swift
+//  PokemonTCG
+//
+//  Created by 김시종 on 4/26/25.
+//
+
+import SwiftUI
+
+@main
+struct PokemonTCGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PokemonTCGTests/PokemonTCGTests.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonTCGTests.swift
+//  PokemonTCGTests
+//
+//  Created by 김시종 on 4/26/25.
+//
+
+import Testing
+@testable import PokemonTCG
+
+struct PokemonTCGTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
